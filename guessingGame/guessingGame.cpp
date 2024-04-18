73fbf171915b4d2ad224eadb8cecbb0bfe1a9d2b
ParisiBR <parisi18@unifesp.br>
2024-04-18 16:20:12 -0300

    Guessing game - final version

--- guessingGame/guessingGame.cpp
+++ guessingGame/guessingGame.cpp
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 bool checkGuessing(int player_guess, int secret_number){
     // Check if the player guess is correct
@@ -31,28 +33,70 @@ double calculateScore(int player_guess, int secret_number, double &score){
     return score;
 }
 
+int setDificultyLevel(char difficulty_level){
+    int max_attempts = 0;
+
+    // Set the maximum number of attempts based on the difficulty level
+    switch (difficulty_level)
+    {
+    case 'E':
+        max_attempts = 15;
+        break;
+    case 'M':
+        max_attempts = 10;
+        break;
+    case 'H':
+        max_attempts = 5;
+        break;
+    default:
+        std::cout << "Invalid difficulty level. Setting to easy." << std::endl;
+        max_attempts = 15;
+        break;
+    }
+
+    return max_attempts;
+}
+
 int main(){
-    
+    // Set the random seed
+    srand(time(NULL));
+
+    // Set the secret number
+    const int SECRET_NUMBER = rand()%100;
+
+    // Set the initial number of attempts
+    int attempts = 0;
+
+    // Set the player guess
+    int player_guess;
+
+    // Set the initial guess as false
+    bool is_guess_correct = false;
+
+    // Set the initial score
+    double score = 1000.0;
+
+    // Set the difficulty level
+    char difficulty_level;
+
+    // Set the maximum number of attempts
+    int max_attempts = 0;
+
+
     std::cout << "*********************************" << std::endl;
     std::cout << "* Welcome to the guessing game! *" << std::endl;
     std::cout << "*********************************" << std::endl;
 
-    // Set the secret number
-    const int SECRET_NUMBER = 42;
-    // Set the initial number of attempts
-    int attempts = 0;
-    // Set the player guess
-    int player_guess;
-    // Set the initial guess as false
-    bool is_guess_correct = false;
-    // Set the initial score
-    double score = 1000.0;
+    std::cout << "Choose your difficulty level:" << std::endl;
+    std::cout << "Easy (E), Medium (M) or Hard (H)" << std::endl;
+    std::cin >> difficulty_level;
+
+    // Set the maximum number of attempts based on the difficulty level
+    max_attempts = setDificultyLevel(difficulty_level);
 
     // Loop until the player guess the secret number
-    while (!is_guess_correct)
-    {
-        // Increment the number of attempts
-        attempts++;
+
+    for(attempts = 1; attempts <= max_attempts; attempts++){
         // Print the current attempt
         std::cout << "Attempt " << attempts << std::endl;
         // Ask the player to enter a guess
@@ -62,7 +106,11 @@ int main(){
         std::cout << "Your guess was: " << player_guess << std::endl;
 
         // Check if the player guess is correct
-        is_guess_correct = checkGuessing(player_guess, SECRET_NUMBER);
+        if(checkGuessing(player_guess, SECRET_NUMBER))
+        {
+            is_guess_correct = true;
+            break;
+        }
         // Calculate the score
         score = calculateScore(player_guess, SECRET_NUMBER, score);
         // Set the precision to 2 decimal places
@@ -72,9 +120,18 @@ int main(){
         std::cout << "Your current score is: " << score << std::endl;
     }
 
-    std::cout << "End of the game!" << std::endl;
-    std::cout << "You took " << attempts << " attempts to guess the secret number." << std::endl;
+    std::cout << "\nEnd of the game!" << std::endl;
+
+    if(!is_guess_correct){
+        std::cout << "You've lost the game! Try again!" << std::endl;
+        std::cout << "You didn't guess the secret number." << std::endl;
+    }else{
+        std::cout << "You guessed the secret number in " << attempts << " attempts." << std::endl;
+        std::cout << "You took " << attempts << " attempts to guess the secret number." << std::endl;
+    }
+        
     std::cout << "Your final score is: " << score << std::endl;
+    
 
     return 0;
 }
