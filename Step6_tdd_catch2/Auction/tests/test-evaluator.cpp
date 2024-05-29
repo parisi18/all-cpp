e50769b6477a4b6b7745e66a5679b9d6e7f80820
ParisiBR <parisi18@unifesp.br>
2024-05-29 15:56:54 -0300

    Test steps

--- Step6_tdd_catch2/Auction/tests/test-evaluator.cpp
+++ Step6_tdd_catch2/Auction/tests/test-evaluator.cpp
@@ -5,16 +5,25 @@
 
 int main(){
 
+    //Arrange - Given
     Bid bid2(User("Lud Chagas"), 2000);
     Bid bid1(User("Rafael Parisi"), 1000);
     Auction auction("Playstation 5");
     auction.receive_bid(bid1);
     auction.receive_bid(bid2);
 
+    //Act - When
     Evaluator evaluator;
     evaluator.evaluate(auction);
 
-    std::cout << "Highest bid: " << evaluator.get_highest_value() << "\n"; // Expected output: "Highest bid: 2000
+    //Assert - Then
+    float expected_value = 2000;
+
+    if(evaluator.get_highest_value() == expected_value){
+        std::cout << "Test passed\n";
+    } else {
+        std::cout << "Test failed\n";
+    }
 
     return 0;
 }
