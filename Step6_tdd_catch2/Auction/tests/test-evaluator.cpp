e08b658d6e9a5c13decda8320975a369a9816707
ParisiBR <parisi18@unifesp.br>
2024-05-30 11:35:23 -0300

    CMake config

--- Step6_tdd_catch2/Auction/tests/test-evaluator.cpp
+++ Step6_tdd_catch2/Auction/tests/test-evaluator.cpp
@@ -1,29 +1,26 @@
-#include <iostream>
-#include "Bid.hpp"
-#include "Auction.hpp"
-#include "Evaluator.hpp"
+// #define CATCH_CONFIG_MAIN
 
-int main(){
+// int main(){
 
-    //Arrange - Given
-    Bid bid2(User("Lud Chagas"), 2000);
-    Bid bid1(User("Rafael Parisi"), 1000);
-    Auction auction("Playstation 5");
-    auction.receive_bid(bid1);
-    auction.receive_bid(bid2);
+//     //Arrange - Given
+//     Bid bid2(User("Lud Chagas"), 2000);
+//     Bid bid1(User("Rafael Parisi"), 1000);
+//     Auction auction("Playstation 5");
+//     auction.receive_bid(bid1);
+//     auction.receive_bid(bid2);
 
-    //Act - When
-    Evaluator evaluator;
-    evaluator.evaluate(auction);
+//     //Act - When
+//     Evaluator evaluator;
+//     evaluator.evaluate(auction);
 
-    //Assert - Then
-    float expected_value = 2000;
+//     //Assert - Then
+//     float expected_value = 2000;
 
-    if(evaluator.get_highest_value() == expected_value){
-        std::cout << "Test passed\n";
-    } else {
-        std::cout << "Test failed\n";
-    }
+//     if(evaluator.get_highest_value() == expected_value){
+//         std::cout << "Test passed\n";
+//     } else {
+//         std::cout << "Test failed\n";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
