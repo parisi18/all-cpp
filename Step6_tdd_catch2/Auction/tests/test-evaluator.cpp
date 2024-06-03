7cbf6a13745e64eb750a0dbc42a45653d9ca3aa5
ParisiBR <parisi18@unifesp.br>
2024-06-03 18:36:16 -0300

    top 3 highest bid order test

--- Step6_tdd_catch2/Auction/tests/test-evaluator.cpp
+++ Step6_tdd_catch2/Auction/tests/test-evaluator.cpp
@@ -67,3 +67,28 @@ TEST_CASE("Should return the lowest ascending order bids"){
     REQUIRE(1000 == evaluator.get_lowest_value());
 }
 
+TEST_CASE("Should return the top three highest bids"){
+    //Arrange - Given
+    Bid bid2(User("Lud Chagas"), 1000);
+    Bid bid1(User("Rafael Parisi"), 2000);
+    Bid bid3(User("John Doe"), 1500);
+    Bid bid4(User("Jane Doe"), 2500);
+
+    Auction auction("Playstation 5");
+    auction.receive_bid(bid1);
+    auction.receive_bid(bid2);
+    auction.receive_bid(bid3);
+    auction.receive_bid(bid4);
+
+    //Act - When
+    Evaluator evaluator;
+    evaluator.evaluate(auction);
+
+
+    //Assert - Then
+    auto top_3_bids = evaluator.get_top_3_bids();
+    REQUIRE(3 == evaluator.get_top_3_bids().size());
+    REQUIRE(2500 == top_3_bids[0].get_amount());
+    REQUIRE(2000 == top_3_bids[1].get_amount());
+    REQUIRE(1500 == top_3_bids[2].get_amount());
+}
