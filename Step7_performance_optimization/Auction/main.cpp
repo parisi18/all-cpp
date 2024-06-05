b6106b2ae76f63eec700c018ae2d791d8ba93b64
ParisiBR <parisi18@unifesp.br>
2024-06-05 19:55:59 -0300

    R-value and L-value comment

--- Step7_performance_optimization/Auction/main.cpp
+++ Step7_performance_optimization/Auction/main.cpp
@@ -17,31 +17,38 @@ void show_user_name(std::shared_ptr<User> user){
 }
 
 int main(){
-    return 0;
-}
-
-int main2(){
     std::cout << "---------------------------------" << std::endl;
     
-    // // SSO (Small String Optimization)
-    // // If the string is small enough, it will be stored in the stack
-    // std::string couple = "John Doe & Jane Doe";
+    // *** SSO (Small String Optimization) ***
+    // If the string is small enough, it will be stored in the stack
+    std::string couple = "John Doe & Jane Doe";
 
-    // // The following code will not allocate memory using string_view
-    // //--std=c++17
-    // std::string_view first_name(couple.c_str(), couple.find("&") - 1);
-    // std::string_view second_name(couple.c_str() + couple.find("&") + 2);
+    // The following code will not allocate memory using string_view
+    //--std=c++17
+    std::string_view first_name(couple.c_str(), couple.find("&") - 1);
+    std::string_view second_name(couple.c_str() + couple.find("&") + 2);
 
-    // show_name(first_name);
-    // show_name(second_name);
+    show_name(first_name);
+    show_name(second_name);
 
-    // // A reference to a char pointer will not allocate memory
-    // show_name("John Doe");
+    // A reference to a char pointer will not allocate memory
+    show_name("John Doe");
     
-    //std::unique_ptr<User> user_unique = std::make_unique<User>(User("John Doe"));
+    // *** Smart Pointers ***
+    std::unique_ptr<User> user_unique = std::make_unique<User>(User("John Doe"));
     std::shared_ptr<User> user_shared = std::make_shared<User>("John Doe");
 
     show_user_name(user_shared);
+
+    // L-value reference
+    // What is an L-value reference?
+    // An L-value reference is a reference that binds to an L-value.
+    // An L-value is an object that occupies some identifiable location in memory (i.e., has an address).
+
+    // R-value reference
+    // What is an R-value reference?
+    // An R-value reference is a reference that binds to an R-value.
+    // An R-value is an object that DOES NOT occupy an identifiable location in memory (i.e., does not have an address).
     
     return 0;
 }
