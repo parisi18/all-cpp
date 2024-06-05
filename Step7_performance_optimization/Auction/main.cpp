14838f15fae052ac95230b076515433c10dc91f5
ParisiBR <parisi18@unifesp.br>
2024-06-05 20:52:02 -0300

    Move semantics examples

--- Step7_performance_optimization/Auction/main.cpp
+++ Step7_performance_optimization/Auction/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <string.h>
 
 #include "User.hpp"
 #include "Bid.hpp"
@@ -18,11 +19,50 @@ void show_user_name(std::shared_ptr<User> user){
     std::cout << user->get_name() << std::endl;
 }
 
+class MyString{
+private:
+    char* m_buffer;
+    size_t size;
+public:
+    MyString(const char* string){
+        size = strlen(string);
+        m_buffer = new char[size];
+        memcpy(m_buffer, string, size);
+    }
+
+    MyString(const MyString& other){ // Copy constructor
+        std::cout << "Copied string" << std::endl;
+        size = other.size;
+        m_buffer = new char[size];
+        memcpy(m_buffer, other.m_buffer, size);
+    }
+
+    MyString(MyString&& other){ // Move constructor
+        std::cout << "Moved string" << std::endl;
+        size = other.size;
+        m_buffer = other.m_buffer;
+        other.size = 0;
+        other.m_buffer = nullptr;
+    }
+
+    ~MyString(){
+        delete m_buffer;
+    }
+};
+
+class MyUser{
+private:
+    MyString m_name;
+public:
+    MyUser(const MyString& user_name) : m_name(user_name){} // Copy constructor
+    MyUser(MyString&& user_name) : m_name((MyString&&) user_name){} // Move constructor
+};
+
 int main(){
     std::cout << "---------------------------------" << std::endl;
     
     // *** SSO (Small String Optimization) ***
-
+    std::cout << "SSO (Small String Optimization)" << std::endl;
     // If the string is small enough, it will be stored in the stack
     std::string couple = "John Doe & Jane Doe";
 
@@ -38,13 +78,14 @@ int main(){
     show_name("John Doe");
     
     // *** Smart Pointers ***
+    std::cout << "\nSmart Pointers" << std::endl;
     std::unique_ptr<User> user_unique = std::make_unique<User>(User("John Doe"));
     std::shared_ptr<User> user_shared = std::make_shared<User>("John Doe");
 
     show_user_name(user_shared);
 
     // *** Move Semantics ***
-
+    std::cout << "\nMove Semantics" << std::endl;
     // L-value reference
     // What is an L-value reference?
     // An L-value reference is a reference that binds to an L-value.
@@ -63,5 +104,15 @@ int main(){
     auction.receive_bid(Bid(User("John Doe"), 300)); // r-value reference
 
 
+    // *** Copy Elision ***
+    std::cout << "\nCopy Elision" << std::endl;
+    // Copy elision is a compiler optimization technique that eliminates unnecessary copying of objects.
+    // In C++, the compiler is allowed to eliminate the copy/move constructor and destructor calls in certain situations.
+    // This optimization is called copy elision.
+    // Copy elision is mandatory in C++17, but it is optional in C++11 and C++14.
+
+    MyUser(MyString("John Doe"));
+
     return 0;
 }
+
